a1d940d9c73870c939d8d294ef5095e184bb4a56
Sevi Goulandri <sevigouland@gmail.com>
2025-09-06 21:27:05 +0300

    implement rate limiting per IP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // custom throttle for submit survey per IP
+        RateLimiter::for('submit-survey', function ($request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
+        // custom throttle for getting current logged-in responder details per IP
+        RateLimiter::for('me', function ($request) {
+            return Limit::perMinute(10)->by($request->ip());
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,12 +10,17 @@ use App\Http\Controllers\ResponderController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/surveys', [SurveyController::class, 'index']);
-Route::get('/surveys/{id}', [SurveyController::class, 'show']);
+Route::get('/surveys', [SurveyController::class, 'index'])
+    ->middleware('throttle:20,1'); // 30 requests per minute per IP
+
+Route::get('/surveys/{id}', [SurveyController::class, 'show'])
+    ->middleware('throttle:20,1');
 
 // protected with JWT
-Route::middleware('jwt.auth')->group(function () {
-    Route::post('/surveys/{id}/submit', [SurveyController::class, 'submit']);
-    Route::get('/me', [ResponderController::class, 'me']);
-});
+Route::post('/surveys/{id}/submit', [SurveyController::class, 'submit'])
+    ->middleware(['jwt.auth', 'throttle:submit-survey']);// rate limit for 5 requests per minute (AppServiceProvider)
+
+Route::get('/me', [ResponderController::class, 'me'])
+->middleware(['jwt.auth', 'throttle:me']);
+
 
