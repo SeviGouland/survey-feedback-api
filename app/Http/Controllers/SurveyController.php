756986c361f545599d5a0f484374c59a0d24156a
Sevi Goulandri <sevigouland@gmail.com>
2025-09-06 18:38:40 +0300

    methods on the Survey controller for the endpoints that don't require JWT

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Survey;
+
 
 class SurveyController extends Controller
 {
-    //
+
+    // get list with all the active surveys
+    public function index()
+    {
+        $surveys = Survey::where('status', '=', 'active')->get();
+
+        return response()->json([
+            'data' => $surveys
+        ]);
+    }
+
+    // get survey details with questions
+    public function show($id)
+    {
+        $survey = Survey::with('questions')
+            ->where('id', $id)
+            ->firstOrFail(); // 404 if it doesn't exist
+
+        return response()->json([
+            'data' => $survey
+        ]);
+    }
+
 }
