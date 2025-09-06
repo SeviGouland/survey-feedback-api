289c6509b6632c3589d5f01dd76c8f29a3d16dae
Sevi Goulandri <sevigouland@gmail.com>
2025-09-06 21:25:45 +0300

    method on responder controller for getting the current logged-in responder details

--- app/Http/Controllers/ResponderController.php
+++ app/Http/Controllers/ResponderController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Responder;
 
 class ResponderController extends Controller
 {
-    //
+    public function me(Request $request)
+    {
+        $responder = $request->user(); 
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'id' => $responder->id,
+                'email' => $responder->email,
+            ]
+        ]);
+    }
 }
